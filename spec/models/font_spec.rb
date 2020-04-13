36c0ac9938838e9e3a5f4be37fae51cc70da9a26
konyan <devkonyan@gmail.com>
2020-04-14 02:36:53 +0630

    fix rubocop

--- spec/models/font_spec.rb
+++ spec/models/font_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Font, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Font.new(name: "notosan",origin_name:"Nato San")
+  end
+
+  it "Font should be valid" do
+    expect(subject).to be_valid
+  end
+
+  it "Font name should present" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "Font orgin name should be present" do
+    subject.origin_name = nil
+    expect(subject).to_not be_valid
+  end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Image.new(name: "Rain",image_url:"U4U2Nd1PoTQ")
+  end
+
+  it "Image should be valid" do
+    expect(subject).to be_valid
+  end
+
+  it "Image name should present" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "Image url should be present" do
+    subject.image_url = nil
+    expect(subject).to_not be_valid
+  end
 end
